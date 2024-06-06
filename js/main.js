58ed70fcb7627e336a79ecf5eba6043eb65c7f8b
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-06-07 03:57:36 +0500

    Homework3-3

---
+++ js/main.js
@@ -0,0 +1,129 @@
+// {
+//   1 - masala;
+//   let a = +prompt("Son kiriting");
+//   if (a > 0) {
+//     alert(`Soningiz musbat bolgani uchun 1 qoshildi ` + (a + 1));
+//   } else {
+//     alert("Soningiz manfiy bolgani uchun shunday qaytarildi " + a);
+//   }
+// }
+
+// {
+//     2-masala
+//     let a = +prompt("Son kiriting");
+//       if (a > 0) {
+//         alert(`Soningiz musbat bolgani uchun 1 qoshildi ` + (a + 1));
+//       } else {
+//         alert("Soningiz manfiy bolgani uchun 2 ga kamaydi " + (a - 2) );
+//       }
+// }
+
+// {
+//   3 - masala;
+//   let a = +prompt("Son kiriting");
+//   if (a > 0) {
+//     alert(`Soningiz musbat bolgani uchun 1 qoshildi ` + (a + 1));
+//   } else if (a == 0) {
+//     alert(" Soningiz 0 bolgani uchun 10 ga ozgartirildi " + (a + 10));
+//   } else if (a < 0) {
+//     alert("Soningiz manfiy bolgani uchun 2 ga kamaydi " + (a - 2));
+//   }
+// }
+
+// {
+//   4 - masala;
+//   let a = +prompt("1- sonni kiriting");
+//   let b = +prompt("2- sonni kiriting");
+//   let c = +prompt("3- sonni kiriting");
+//   let musbat = 0;
+//   if (a > 0) {
+//     musbat++;
+//   }
+//   if (b > 0) {
+//     musbat++;
+//   }
+//   if (c > 0) {
+//     musbat++;
+//   }
+//   alert("Sonlaringiz orasida " + musbat + " ta musbat son bor ");
+// }
+
+// {
+//   5 - masala;
+//   let a = +prompt("1- sonni kiriting");
+//   let b = +prompt("2- sonni kiriting");
+//   let c = +prompt("3- sonni kiriting");
+//   let musbat = 0;
+//   let manfiy = 0;
+//   if (a > 0) {
+//     musbat++;
+//   } else {
+//     manfiy++;
+//   }
+//   if (b > 0) {
+//     musbat++;
+//   } else {
+//     manfiy++;
+//   }
+//   if (c > 0) {
+//     musbat++;
+//   } else {
+//     manfiy++;
+//   }
+//   alert(
+//     "Sonlaringiz orasida " +
+//       musbat +
+//       " ta musbat va " +
+//       manfiy +
+//       " ta manfiy son bor "
+//   );
+// }
+
+// {
+//   6 - masala;
+//   let a = +prompt("1- sonni kiriting");
+//   let b = +prompt("2- sonni kiriting");
+//   if (a > b) {
+//     alert("Sonlaringiz orasida " + a + " Kattaroq ");
+//   } else {
+//     alert("Sonlaringiz orasida " + b + " Kattaroq ");
+//   }
+// }
+
+// {
+//   8 - masala;
+//   let a = +prompt("1- sonni kiriting");
+//   let b = +prompt("2- sonni kiriting");
+//   if (a > b) {
+//     alert("Sonlaringiz orasida " + a + " Kattaroq ");
+//     alert("Sonlaringiz orasida " + b + " Kichkinaroq ");
+//   } else {
+//     alert("Sonlaringiz orasida " + b + " Kattaroq ");
+//     alert("Sonlaringiz orasida " + a + " Kichkinaroq ");
+//   }
+// }
+
+// {
+//     9-masala
+//     let a = +prompt("a uchun son kiriting")
+//     let b = +prompt("b uchun son kiriting")
+//     a = 1
+//     b = 2
+//     if(a < b ){
+//         alert(" a soni b dan kichkina chunki " + a  +"  kichkina  " + b + " dan ")
+//         alert(" Boshidan urinib koring 😂😂 ")
+//     }
+// }
+
+// {
+//   10 - masala;
+//   let a = +prompt(" a uchun son kiriting ");
+//   let b = +prompt(" b uchun son kiriting ");
+//   if (a == b) {
+//     alert(" Sonlaringiz teng bolgani sababli ularni ayirib tashladik " + 0);
+//   } else {
+//     alert(
+//       " Sonlaringiz teng bolmaganligi sababli ularni yegindisi  " + (a + b)
+//     );
+//   }
+// }
